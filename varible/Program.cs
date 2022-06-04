2955e34c97e0e7cd7e8d78245b080f59a62eef26
sezahedi <sezahedi13@gmail.com>
2022-06-04 18:55:37 +0430

    Add project files.

---
+++ varible/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varible
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Hedi";
+            var age = 21;
+            var isStudent = true;
+            Type nametype = name.GetType();
+            Type agetype = age.GetType();
+            Type isStudenttype = isStudent.GetType();
+            Console.WriteLine($"name is of type{nametype}");
+            Console.WriteLine($"age is of type{agetype}");
+            Console.WriteLine($"isstudent is of type{isStudenttype}");
+            Console.ReadKey();
+        }
+    }
+}
